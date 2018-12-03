55b6fdd3d92b2576d35d66881b032669cec3a9a2
yang-seon Kim <pinkrespect@jj.ac.kr>
2018-12-03 13:20:58 +0900

    Make Upload.vue

--- bird-house/src/views/Upload.vue
+++ bird-house/src/views/Upload.vue
@@ -1,16 +1,99 @@
 <template>
-  <div class="upload">
+  <div class="container">
     <div class="large-12 medium-12 small-12 cell">
-      <label>File
-        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      <label>Files
+        <input type="file" id="files" ref="files" multiple v-on:change="handleFilesUpload()"/>
       </label>
-      <button v-on:click="submitFile()">Submit</button>
+    </div>
+    <div class="large-12 medium-12 small-12 cell">
+      <div v-for="(file, key) in files" class="file-listing">{{ file.name }} <span class="remove-file" v-on:click="removeFile( key )">Remove</span></div>
+    </div>
+    <br>
+    <div class="large-12 medium-12 small-12 cell">
+      <button v-on:click="addFiles()">Add Files</button>
+    </div>
+    <br>
+    <div class="large-12 medium-12 small-12 cell">
+      <button v-on:click="submitFiles()">Submit</button>
     </div>
   </div>
-
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+  /*
+      Defines the data used by the component
+   */
+  data () {
+    return {
+      files: []
+    }
+  },
+  /*
+      Defines the method used by the component
+   */
+  methods: {
+    /*
+        Adds a file
+     */
+    addFiles () {
+      this.$refs.files.click()
+    },
+    /*
+        Submits files to the server
+     */
+    submitFiles () {
+      /*
+          Initialize the form data
+       */
+      let formData = new FormData()
+      /*
+          Iteate over any file sent over appending the files
+          to the form data.
+       */
+      for (var i = 0; i < this.files.length; i++) {
+        let file = this.files[i]
+        formData.append('files[' + i + ']', file)
+      }
+      /*
+          Make the request to the POST /select-files URL
+       */
+      axios.post('/photos',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+      ).then(function () {
+        console.log('SUCCESS!!')
+      })
+        .catch(function () {
+          console.log('FAILURE!!')
+        })
+    },
+    /*
+        Handles the uploading of files
+     */
+    handleFilesUpload () {
+      let uploadedFiles = this.$refs.files.files
+      /*
+          Adds the uploaded file to the files array
+       */
+      for (var i = 0; i < uploadedFiles.length; i++) {
+        this.files.push(uploadedFiles[i])
+      }
+    },
+    /*
+        Removes a select file the user has uploaded
+     */
+    removeFile (key) {
+      this.files.splice(key, 1)
+    }
+  }
+}
 </script>
 
 <style scoped>
